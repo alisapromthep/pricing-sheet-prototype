f1938e58e9e1a758ffde1e55d1f53d69c4bb9c1c
alisapromthep <alisa.promthep@gmail.com>
2024-11-15 11:48:29 -0800

    options forms display

--- src/app/components/OptionsForm/OptionsForm.tsx
+++ src/app/components/OptionsForm/OptionsForm.tsx
@@ -11,7 +11,22 @@ type FormProps = {
 const OptionsForm: React.FC<FormProps> = ({ optionsData, labelsArray }) => {
   const optionObject = organizeOptionsData(optionsData);
   console.log(optionObject);
-  return <form>{}</form>;
+  return (
+    <form className="flex flex-col">
+      {labelsArray.map((label, i) => {
+        return (
+          <label key={i}>
+            {label}
+            <select>
+              {optionsData.map((option, k) => (
+                <option key={k}>{option[i]}</option>
+              ))}
+            </select>
+          </label>
+        );
+      })}
+    </form>
+  );
 };
 
 export default OptionsForm;
